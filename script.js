6778a33d973c6158eae0f1f8ae86ee46217cf2b0
Md. Sayed Uddin Rayhan <67170635+SayedUddinRayhan@users.noreply.github.com>
2023-03-26 05:45:30 +0000

    Added 3 functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+let mobile = document.querySelector("#mobile");
 
+let name2 = document.querySelector("#name2");
+let email2 = document.querySelector("#email2");
+let mobile2 = document.querySelector("#mobile2");
+
+name.addEventListener("keyup", function() {
+  name2.innerText = name.value;
+})
+
+email.addEventListener("keyup", function() {
+  email2.innerText = email.value;
+})
+
+mobile.addEventListener("keyup", function() {
+  mobile2.innerText = mobile.value;
+})
